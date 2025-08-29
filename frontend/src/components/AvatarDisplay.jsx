b7511d55d5b8dfb7ab4a730fdba56ece7725ae4d
Chavi Satish Jain <cjain@cjainV4JQ6.omnissa.com>
2025-08-29 22:13:37 +0530

    re-working on frontend

--- frontend/src/components/AvatarDisplay.jsx
+++
@@ -1,7 +0,0 @@
-export default function AvatarDisplay() {
-  return (
-    <div className="mb-4">
-      <img src="/avatar.png" alt="Avatar" className="w-32 h-32 rounded-full border-4 border-white" />
-    </div>
-  )
-}
--- frontend/src/components/VoiceButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function VoiceButton() {
-  return (
-    <button className="bg-purple-600 px-4 py-2 rounded mt-4">
-      🎤 Voice Input (Coming Soon)
-    </button>
-  )
-}
